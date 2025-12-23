72d111823e2822f0dcf924c5c3125a0e9cc2b194
Samosvatova Anastasiia <anastasiia.samosvatova@nure.ua>
2025-12-23 11:14:26 +0200

    Delete Lab3/ark-pzpi-23-5-samosvatova-anastasiia-lab3/agrosense-api/backend directory

--- Lab3/ark-pzpi-23-5-samosvatova-anastasiia-lab3/agrosense-api/backend/src/middleware/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode).json({
-        error: err.message,
-    });
-};
-
-module.exports = { errorHandler };
--- Lab3/ark-pzpi-23-5-samosvatova-anastasiia-lab3/agrosense-api/backend/src/utils/tokenService.js
+++
@@ -1,3 +0,0 @@
-const jwt = require('jsonwebtoken');
-const generateToken = (id, role) => jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '24h' });
-module.exports = { generateToken };
